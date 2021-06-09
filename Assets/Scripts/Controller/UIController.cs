9a5ee0ec73f44c5a0a8effec59b1ea0a8ca4a080
modty <2673077461@qq.com>
2021-06-09 08:56:41 +0800

    2021-06-09 添加UI

---
+++ Assets/Scripts/Controller/UIController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using ActionPool;
+using Managers;
+using UnityEngine;
+
+namespace Scripts.Controller
+{
+    public class UIController:MonoBehaviour
+    {
+        private static UIController _instance;
+        public static UIController Instance => _instance;
+
+        private CombatTextManager combatTextManager;
+        private Dictionary<long, HeadBarController> _headBars;
+        [SerializeField] private GameObject _headBarParent;
+        private void Awake()
+        {
+            _instance = this;
+            _headBars = new Dictionary<long, HeadBarController>();
+            combatTextManager=CombatTextManager.Instance;
+        }
+
+        public GameObject TargetUI;
+        
+        public void AddHeadBar(PlayerAttribute characterAttribute)
+        {
+            GameObject headbar = PrefabsManager.Instance.HeadBar;
+            HeadBarController barController = headbar.GetComponent<HeadBarController>();
+            barController.CharacterAttribute = characterAttribute;
+            barController.HeadBar = headbar;
+            headbar.name = characterAttribute.Uid.ToString();
+            headbar.transform.SetParent(_headBarParent.transform);
+            _headBars[characterAttribute.Uid] = barController;
+        }
+
+        public void RemoveHeadBar(PlayerAttribute characterAttribute)
+        {
+            Destroy(_headBars[characterAttribute.Uid].gameObject);
+            _headBars.Remove(characterAttribute.Uid);
+        }
+    }
+}
