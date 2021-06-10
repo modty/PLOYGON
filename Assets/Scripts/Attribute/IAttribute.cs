b12056f829dec7df441dbe0b8d6d19bb512314d7
modty <2673077461@qq.com>
2021-06-10 10:25:52 +0800

    2021-06-10 增加角色属性类

---
+++ Assets/Scripts/Attribute/IAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Scripts
+{
+    public interface IAttribute
+    {
+        /// <summary>
+        /// 攻击UI显示的字符串
+        /// </summary>
+        /// <returns></returns>
+        string BaseValue();
+        string CurrentValue();
+    }
+}
