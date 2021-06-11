a8dd08bbd42584aa599eda4aa39b02535b44bd04
modty <2673077461@qq.com>
2021-06-11 17:27:18 +0800

    2021-06-11 框架重构

---
+++ Assets/Scripts/Domain/MessageEntities/MouseClickRight.cs
@@ -0,0 +1,32 @@
+﻿using Data;
+using Loxodon.Framework.Messaging;
+using UnityEngine;
+
+namespace Domain.MessageEntities
+{
+    /// <summary>
+    /// 鼠标右键点击信息
+    /// </summary>
+    public class MouseClickRight:MessageBase
+    {
+        private Vector3 _position;
+        private GameData _clickObject;
+        public MouseClickRight(object sender,Vector3 position,GameData clickObject) : base(sender)
+        {
+            _position = position;
+            _clickObject = clickObject;
+        }
+
+        public Vector3 Position
+        {
+            get => _position;
+            set => _position = value;
+        }
+
+        public GameData ClickObject
+        {
+            get => _clickObject;
+            set => _clickObject = value;
+        }
+    }
+}
---
+++ Assets/Scripts/Service/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Managers
+{
+    /// <summary>
+    /// 游戏管理
+    /// </summary>
+    public class GameManager:MonoBehaviour
+    {
+        // 游戏中的所有角色
+        private TheCharacter _character;
+        private void Start()
+        {
+            _character = TheCharacter.Instance;
+        }
+    }
+}
